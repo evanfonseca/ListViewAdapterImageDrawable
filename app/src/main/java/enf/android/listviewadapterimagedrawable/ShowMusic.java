8869f4917f3c24e14b7a24d58ca920b11d61d499
evanfonseca <evanfonseca@gmail.com>
2016-04-18 17:14:57 -0100

    playing music selected. pause

--- app/src/main/java/enf/android/listviewadapterimagedrawable/ShowMusic.java
+++ app/src/main/java/enf/android/listviewadapterimagedrawable/ShowMusic.java
@@ -1,10 +1,14 @@
 package enf.android.listviewadapterimagedrawable;
 
 import android.content.res.Resources;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.SeekBar;
 import android.widget.TextView;
 
 /**
@@ -23,8 +27,23 @@ public class ShowMusic extends AppCompatActivity {
             R.drawable.mayra,
             R.drawable.jhumberto};
 
+    int[] audios ={R.raw.maria_di_lida,
+            R.raw.largam_da_mon,
+            R.raw.mindel_dnovas,
+            R.raw.nha_pretinha,
+            R.raw.sabim,
+            R.raw.triste_eh_sabe,
+            R.raw.nos_tradison,
+            R.raw.lebam_ku_bo,
+            R.raw.ilha_de_santiago,
+            R.raw.strela_cadente};
+
     String [] Titulos;
     String [] Artistas;
+    Button play;
+    boolean bplay=false;
+    SeekBar sb;
+    TextView tvsb;
 
 
 
@@ -41,6 +60,9 @@ public class ShowMusic extends AppCompatActivity {
         TextView myMusica= (TextView) findViewById(R.id.Musica);
         TextView  myArtista= (TextView) findViewById(R.id.Artista);
 
+
+
+
         myImage.setImageResource(images[p]);
         Resources res = getResources();
         Titulos=res.getStringArray(R.array.titulos);
@@ -48,7 +70,38 @@ public class ShowMusic extends AppCompatActivity {
         myMusica.setText(Titulos[p]);
         myArtista.setText(Artistas[p]);
 
+        play= (Button) findViewById(R.id.play);
+        //bplay=false;
+
+        final MediaPlayer mp = MediaPlayer.create(this, audios[p]);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(!bplay){
+                            mp.start();
+                            play.setText("Pause");
+                            bplay=true;
+                          }
+                else      {
+                            mp.pause();
+                            play.setText("Play");
+                            bplay=false;
+                          }
+
+            }
+        });
+
 
 
     }
+
+
+    public  void seekbar(){
+        sb= (SeekBar) findViewById(R.id.seekBar);
+        tvsb= (TextView) findViewById(R.id.seekBarTv);
+        tvsb.setText("SeekBar PROGRESS "+sb.getProgress());
+
+    }
+
 }
